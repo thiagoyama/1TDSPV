5d8ebde4e3ff34bdaf019712f30fb5aa4d726539
Thiago Yamamoto <thiagoyama@gmail.com>
2025-03-14 21:05:04 -0300

    Primeiro projeto, tipos primitivos e atributos

---
+++ Aula01/src/Casa.java
@@ -0,0 +1,12 @@
+public class Casa {
+
+    //8 Tipo primitivos
+    //Definir os atributos (propriedades)
+    //"tipo do dado" "nome do atributo"
+    float temperatura;
+    boolean possuiEncanamento;
+    int senhaPorta;
+    double areaTotal;
+    char tipoResidencia;
+
+}
---
+++ Aula01/src/Terminal.java
@@ -0,0 +1,18 @@
+public class Terminal {
+
+    //Método que inicializa o nosso programa
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+
+        //Criar o objeto Casa
+        Casa bbb = new Casa();
+        bbb.temperatura = 25;
+
+        Casa casaAna = new Casa();
+        casaAna.temperatura = 20;
+
+        Casa casaMatheus = new Casa();
+
+    }
+
+}
