87c858fd639bb38e8e394064aa24d1e228e2c243
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 20:56:16 -0300

    Calculo da Média do Semestre e Inicio da Apostila 04

---
+++ Apostila04/src/br/com/fiap/loja/model/Produto.java
@@ -0,0 +1,10 @@
+package br.com.fiap.loja.model;
+
+public class Produto {
+
+    double preco;
+    int quantidade;
+    String nome;
+    boolean garantia;
+
+}
---
+++ Apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -0,0 +1,13 @@
+package br.com.fiap.loja.view;
+
+public class Terminal {
+
+    public static void main(String[] args) {
+        //Ler os dados do produto
+        //Criar o objeto produto
+        //Atribuir os valores ao objeto
+
+        //Exibir os valores dos atributos
+    }
+
+}
