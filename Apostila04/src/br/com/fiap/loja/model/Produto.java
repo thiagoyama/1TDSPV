fe2445994e7746a258093025d9715f2cd88b72b2
unknown <profthiagoy@fiap.com.br>
2026-03-20 20:57:04 -0300

    Entrada e Saida de dados com dois objetos relacionados

--- Apostila04/src/br/com/fiap/loja/model/Produto.java
+++ Apostila04/src/br/com/fiap/loja/model/Produto.java
@@ -2,9 +2,12 @@ package br.com.fiap.loja.model;
 
 public class Produto {
 
-    double preco;
-    int quantidade;
-    String nome;
-    boolean garantia;
+    //public - modificador de acesso que permite que todos tenham acesso
+    public double preco;
+    public int quantidade;
+    public String nome;
+    public boolean garantia;
+
+    public Categoria categoria;
 
 }
--- Apostila04/src/br/com/fiap/loja/view/Terminal.java
+++ Apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -1,13 +1,62 @@
 package br.com.fiap.loja.view;
 
+import br.com.fiap.loja.model.Categoria;
+import br.com.fiap.loja.model.Produto;
+
+import java.util.Scanner;
+
 public class Terminal {
 
     public static void main(String[] args) {
+        //Objeto para ler os dados do teclado
+        Scanner entrada = new Scanner(System.in);
+
         //Ler os dados do produto
+        System.out.println("Digite o nome do produto");
+        String nome = entrada.next() + entrada.nextLine();
+
+        System.out.println("Digite a quantidade do produto");
+        int quantidade = entrada.nextInt();
+
+        System.out.println("Digite o valor do produto");
+        double valor = entrada.nextDouble();
+
+        System.out.println("Possui garantia (true/false)");
+        boolean garantia = entrada.nextBoolean();
+
+        //Receber os valores da categoria (nome e descrição)
+        System.out.println("Digite o nome da categoria");
+        String nomeCategoria = entrada.next() + entrada.nextLine();
+
+        System.out.println("Digite a descrição da categoria");
+        String descricaoCategoria = entrada.next() + entrada.nextLine();
+
         //Criar o objeto produto
+        Produto produto = new Produto();
+
         //Atribuir os valores ao objeto
+        produto.nome = nome;
+        produto.preco = valor;
+        produto.garantia = garantia;
+        produto.quantidade = quantidade;
+
+        //Criar a categoria
+        Categoria categoria = new Categoria();
+
+        //Adicionar os valores nos atributos da categoria
+        categoria.nome = nomeCategoria;
+        categoria.descricao = descricaoCategoria;
+
+        //Colocar a categoria no produto
+        produto.categoria = categoria;
 
         //Exibir os valores dos atributos
-    }
+        System.out.println("Dados do produto:");
+        System.out.println(produto.nome + ", quantidade: " + produto.quantidade);
+        System.out.println("R$ " + produto.preco + ", tem garantia: " + produto.garantia);
+        System.out.println("Categoria: " + produto.categoria.nome + ", descrição: " + produto.categoria.descricao);
 
+        entrada.close();
+
+    }
 }
