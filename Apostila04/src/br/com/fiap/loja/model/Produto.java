1b74ea4c045f5b4313a710707879aa208a1a15c0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 20:49:02 -0300

    Métodos

--- Apostila04/src/br/com/fiap/loja/model/Produto.java
+++ Apostila04/src/br/com/fiap/loja/model/Produto.java
@@ -7,7 +7,29 @@ public class Produto {
     public int quantidade;
     public String nome;
     public boolean garantia;
-
     public Categoria categoria;
+
+    public double calcularDesconto(){
+        //calcular o valor do produto com 10% de desconto
+        double precoComDesconto = preco * 0.9;
+        return precoComDesconto;
+        //return preco * 0.9;
+    }
+
+    public void alterarPreco(double novoPreco){
+        preco = novoPreco;
+    }
+
+    public double calcularDescontoPorQuantidade(int qtd){
+        double valorFinal;
+        if (qtd >= 5 && qtd < 10){
+            valorFinal = preco * 0.85;
+        } else if (qtd >= 10){
+            valorFinal = preco * 0.8;
+        } else {
+            valorFinal = preco * 0.9;
+        }
+        return valorFinal;
+    }
 
 }
--- Apostila04/src/br/com/fiap/loja/view/Terminal.java
+++ Apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -56,6 +56,23 @@ public class Terminal {
         System.out.println("R$ " + produto.preco + ", tem garantia: " + produto.garantia);
         System.out.println("Categoria: " + produto.categoria.nome + ", descrição: " + produto.categoria.descricao);
 
+        //Receber o valor do produto com o desconto
+        double churros = produto.calcularDesconto();
+        System.out.println("Valor promocional: " + churros);
+
+        //alterar o valor do produto
+        System.out.println("Digite o novo valor do produto:");
+        double precoNovo = entrada.nextDouble();
+        produto.alterarPreco(precoNovo);
+        System.out.println("Novo preco: " + produto.preco);
+
+        //Ler a quantidade de produto que o cliente quer comprar
+        System.out.println("Quantos itens deseja comprar?");
+        int qtdItens = entrada.nextInt();
+        //Calcular o valor com o desconto
+        double valorFinal = produto.calcularDescontoPorQuantidade(qtdItens);
+        //Exibir o valor
+        System.out.println("Valor final com desconto: " + valorFinal);
         entrada.close();
 
     }
